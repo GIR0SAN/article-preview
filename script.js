f4afcafde6a2ada7da1bb02d45c5b4aa52e6334e
GiroSan <girosan86@gmail.com>
2023-07-07 12:01:17 -0300

    Inicio JS

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('ola')
+document.getElementById('links').onclick = function(show){ 
+    document.getElementById("show").style.display = "flex"
+}
+
+document.getElementById('hide').onclick = function(hide){
+    document.getElementById("show").style.display = "none"
+}
